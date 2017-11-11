e2be35caae78d8158ede1a6dd4bbeea24b55dbbb
iver56 <iver56@hotmail.com>
2017-11-11 13:23:49 +0100

    Add placeholder pages for songs list and the player

--- songs/views.py
+++ songs/views.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
 
-# Create your views here.
+
+def index(request):
+    template = get_template('index.html')
+    context = {}
+    html = template.render(context)
+    return HttpResponse(html)
+
+
+def player(request):
+    template = get_template('player.html')
+    context = {}
+    html = template.render(context)
+    return HttpResponse(html)
