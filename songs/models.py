ce2a5c576f45c3c2057dbf55735b1c94b8c859c5
iver56 <iver56@hotmail.com>
2017-11-11 12:59:36 +0100

    Add time_created and time_updated

--- songs/models.py
+++ songs/models.py
@@ -7,3 +7,5 @@ from django.db import models
 class Song(models.Model):
     name = models.CharField(max_length=128)
     midi_file = models.FileField()
+    time_created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True, blank=True)
+    time_updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True, blank=True)
