674bdb9b62c76de171c65e7a66fb91a198648566
iver56 <iver56@hotmail.com>
2017-11-13 23:44:02 +0100

    Remove unused function findGetParameter

--- songs/static/src/helpers.js
+++ songs/static/src/helpers.js
@@ -1,16 +1,3 @@
-function findGetParameter(parameterName) {
-    let result = null,
-        tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function(item) {
-            tmp = item.split("=");
-            if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
-    return result;
-}
-
 function relativeMouseCoords(e, canvas) {
   if (e.type !== "touchstart") {
     e.preventDefault();
