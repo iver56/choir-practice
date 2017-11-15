56ec842b6f750a24248a6f04b3f1582eaa8b3980
iver56 <iver56@hotmail.com>
2017-11-15 18:28:39 +0100

    Swap position of midi file and name in django admin song form

--- songs/models.py
+++ songs/models.py
@@ -5,8 +5,8 @@ from django.db import models
 
 
 class Song(models.Model):
-    name = models.CharField(max_length=128)
     midi_file = models.FileField()
+    name = models.CharField(max_length=128)
     note_pdf_file = models.FileField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True, blank=True)
     time_updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True, blank=True)
