9fc2af1554d251c5466d478214766eed2fd6907f
iver56 <iver56@hotmail.com>
2020-02-22 16:46:19 +0100

    Enable error logging to stdout when DEBUG=False

--- api/settings/base.py
+++ api/settings/base.py
@@ -126,3 +126,58 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+# The following logging config is based on Django's default config:
+# https://github.com/django/django/blob/e9f9915ba8cbff7c8f9f0818fd31cde60cb7ee7a/django/utils/log.py#L16
+# What is different: We enable error logging to console/stdout when DEBUG=False (i.e. in production)
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[%(server_time)s] %(message)s',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'console_debug_false': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'console_debug_false', 'mail_admins'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    }
+}
